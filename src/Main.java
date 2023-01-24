471360f74a5049d60e5c5e0a5fc56a4643a685e1
Robles <joseroblesarce@yahoo.es>
2023-01-24 18:16:57 +0000

    Tarea 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1=0, num2=0, resultado=0;
+
+        Scanner objeto=new Scanner (System.in);
+            System.out.println("Ingresar el primer número de la suma");
+            num1=objeto.nextInt();
+
+            System.out.println("Ingresar el segundo número de la suma");
+            num2=objeto.nextInt();
+
+            resultado=num1+num2;
+
+            System.out.println("El resultado es igual a:"+resultado);
+
+
     }
 }
